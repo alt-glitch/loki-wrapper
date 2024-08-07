c1de5ca35ea630e49af63922e76586fca6252f60
Siddharth Balyan <balyan.sid@gmail.com>
2024-08-07 11:43:16 +0530

    modal+vllm+llama3

--- main.py
+++ main.py
@@ -122,33 +122,9 @@ async def process_query(query: UserQuery):
     logs = loki_client.query_loki(logql_query.query)
 
     return logs
-    #
-    # resp = client.chat.completions.create(
-    #     model="gpt-4-turbo",
-    #     messages=[
-    #         {
-    #             "role": "user",
-    #             "content": "Extract Jason is 25 years old",
-    #         }
-    #     ],
-    #     response_model=User,
-    # )
 
 
 if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-# # Dummy function to convert natural language to LogQL
-# def natural_language_to_logql(query: str) -> str:
-#     # This is a placeholder. In a real implementation, you'd use NLP techniques here.
-#     dummy_conversations = {
-#         "show me all errors": '{container="run_loki-flog-1"} | json status | line_format "{{.status}}" |~ "500"',
-#         "list recent logs": '{container="run_loki-flog-1"}',
-#         "count GET requests in the last hour": '{container="run_loki-flog-1"} | json method | line_format "{{.method}}" |~ "GET" | count_over_time( [1h])',
-#         "show all logs for a specific request": '{container="run_loki-flog-1"} | json request | line_format "{{.request}}" |~ "/proactive/architectures/revolutionary/24%2f365"',
-#     }
-#     return dummy_conversations.get(
-#         query.lower(), '{container="run_loki-flog-1"}'
-#     )
